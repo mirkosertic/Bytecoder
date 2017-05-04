8ad72b3d43d75153808632be6439e24dfda8df6d
Mirko Sertic <mirko.sertic@web.de>
2017-05-04 21:20:50 +0200

    Working on JS Backend.

--- core/src/test/java/de/mirkosertic/bytecoder/core/SimpleMathTest.java
+++ core/src/test/java/de/mirkosertic/bytecoder/core/SimpleMathTest.java
@@ -35,3 +35,35 @@ public class SimpleMathTest {
         }
     }
 }
+
+/*
+public class de.mirkosertic.bytecoder.core.SimpleMathTest {
+  public de.mirkosertic.bytecoder.core.SimpleMathTest();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  public static int sum(int, int);
+    Code:
+       0: iload_0
+       1: iload_1
+       2: iadd
+       3: ireturn
+
+  public void testAdd() throws java.lang.Exception;
+    Code:
+       0: bipush        10
+       2: bipush        20
+       4: invokestatic  #2                  // Method sum:(II)I
+       7: istore_1
+       8: iload_1
+       9: bipush        30
+      11: if_icmpne     22
+      14: new           #3                  // class java/lang/Exception
+      17: dup
+      18: invokespecial #4                  // Method java/lang/Exception."<init>":()V
+      21: athrow
+      22: return
+}
+ */
