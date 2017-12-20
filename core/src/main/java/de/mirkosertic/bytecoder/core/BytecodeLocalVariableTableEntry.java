57c1f3535c2f5f5830be4dfcfce7f27a2a310b59
Mirko Sertic <mirko.sertic@web.de>
2017-12-20 14:05:55 +0100

    #18 Use LocalVariableTable for variable name generation
    Support for reading variable names from bytecode

---
+++ core/src/main/java/de/mirkosertic/bytecoder/core/BytecodeLocalVariableTableEntry.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Mirko Sertic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.mirkosertic.bytecoder.core;
+
+public class BytecodeLocalVariableTableEntry {
+
+    private final int startPC;
+    private final int length;
+    private final int nameIndex;
+    private final BytecodeTypeRef type;
+    private final int index;
+
+    public BytecodeLocalVariableTableEntry(int aStartPC, int aLength, int aNameIndex, BytecodeTypeRef aType, int aIndex) {
+        startPC = aStartPC;
+        length = aLength;
+        nameIndex = aNameIndex;
+        type = aType;
+        index = aIndex;
+    }
+
+    public int getStartPC() {
+        return startPC;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getNameIndex() {
+        return nameIndex;
+    }
+
+    public BytecodeTypeRef getType() {
+        return type;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
