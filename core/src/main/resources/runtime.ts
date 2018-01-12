452e448d08ff950f5a8550eded820aefe9d96ec3
Mirko Sertic <mirko.sertic@web.de>
2018-01-12 21:23:15 +0100

    Typescript test

---
+++ core/src/main/resources/runtime.ts
@@ -0,0 +1,33 @@
+export namespace runtime {
+
+    namespace math {
+
+        function floor(thisref, p1) {
+            return Math.floor(p1);
+        }
+
+        function ceil(thisref, p1) {
+            return Math.ceil(p1);
+        }
+
+        function sin(thisref, p1) {
+            return Math.sin(p1);
+        }
+
+        function cos(thisref, p1) {
+            return Math.cos(p1);
+        }
+
+        function round(thisref, p1) {
+            return Math.round(p1);
+        }
+
+        function float_rem(a, b) {
+            return a % b;
+        }
+
+        function sqrt(thisref, p1) {
+            return Math.sqrt(p1);
+        }
+    }
+}
