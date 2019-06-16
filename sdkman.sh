968397725f19afee0af87b5361fadada7a4babbd
mirkosertic <mirko.sertic@web.de>
2019-06-16 15:42:53 +0200

    #169 Run full integration test suite on Travis CI

--- sdkman.sh
+++ sdkman.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=$HOME/$1
-$TRAVIS_BUILD_DIR/install-jdk.sh --install $1 --target $JAVA_HOME
+~/bin/install-jdk.sh --install $1 --target $JAVA_HOME --workspace "/home/travis/.cache/install-jdk"
 export PATH=$JAVA_HOME/bin:$PATH
