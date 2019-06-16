59114ee212a72c5c4e8c00de6e50ea84c0b3cce8
mirkosertic <mirko.sertic@web.de>
2019-06-16 18:07:18 +0200

    #169 Run full integration test suite on Travis CI

--- jdkman.sh
+++ jdkman.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-if [$1 eq 'openjdk11']; then
-    echo "Installing OpenJDK 11"
-    ~/install-jdk.sh -W /home/travis/openjdk11 -F 11 -L GPL
-fi
-if [$1 eq 'openjdk12']; then
-    echo "Installing OpenJDK 12"
-    ~/install-jdk.sh -W /home/travis/openjdk12 -F 12 -L GPL
-fi
+#!/usr/bin/env bash
+test "$1" == "openjdk11" && (~/install-jdk.sh -W /home/travis/openjdk11 -F 11 -L GPL;)
+test "$1" == "openjdk12" && (~/install-jdk.sh -W /home/travis/openjdk12 -F 12 -L GPL;)
