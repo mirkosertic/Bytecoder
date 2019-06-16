91c6de5f1ab407022db1fef8cb4fd5e597e0ef39
mirkosertic <mirko.sertic@web.de>
2019-06-16 17:22:19 +0200

    #169 Run full integration test suite on Travis CI

--- jdkman.sh
+++ jdkman.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export JAVA_HOME=~/$1
-/home/travis/bin/install-jdk.sh --target "/home/travis/$1" --workspace "/home/travis/.cache/install-jdk" --feature "$2" --license "GPL" --cacerts
+~/install-jdk.sh -W /home/travis/$1 -F $2 -L GPL
 export PATH=$JAVA_HOME/bin:$PATH
