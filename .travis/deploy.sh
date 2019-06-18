3ae1bb0cc97323cd21fe5579d5654e0c330fc041
mirkosertic <mirko.sertic@web.de>
2019-06-18 18:14:05 +0200

    #171 Run release and snapshot Builds from Travis-CI

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+openssl aes-256-cbc -K $encrypted_41bc8c83cf90_key -iv $encrypted_41bc8c83cf90_iv -in .travis/gpg.all.enc -out ./all.gpg -d
+gpg --import ./all.gpg
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
