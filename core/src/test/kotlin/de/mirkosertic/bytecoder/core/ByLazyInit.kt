bc71499a2857277f3f916f87c745618e759f85f5
Mirko Sertic <mirko.sertic@web.de>
2020-04-02 13:19:44 +0200

    #333 Kotlin to WASM - by lazy init does not work
    Added Testcase for by lazy

---
+++ core/src/test/kotlin/de/mirkosertic/bytecoder/core/ByLazyInit.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Mirko Sertic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.mirkosertic.bytecoder.core
+
+import de.mirkosertic.bytecoder.unittest.BytecoderUnitTestRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(BytecoderUnitTestRunner::class)
+class ByLazyInit {
+
+    class EntryKotlin {
+        val me: String by lazy {
+            "Hola"
+        }
+
+        init {
+            println("new entry kotlin")
+            //val nada = Class.forName("")
+        }
+    }
+
+    @Test
+    fun byLazyInit() {
+        val x = EntryKotlin()
+        println(x.me);
+    }
+}
