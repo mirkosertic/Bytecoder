86d8e04be59e26aa089f9d3c4c94d444556f9c0b
mirkosertic <mirko.sertic@web.de>
2020-04-06 13:34:53 +0200

    #331 Resource Bundle: Missing getBundle() methods

---
+++ classlib/java.logging/src/main/java/de/mirkosertic/bytecoder/classlib/java/util/Logging/TLevel.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Mirko Sertic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.mirkosertic.bytecoder.classlib.java.util.Logging;
+
+import de.mirkosertic.bytecoder.api.SubstitutesInClass;
+
+import java.util.logging.Level;
+
+@SubstitutesInClass(completeReplace = true)
+public class TLevel {
+
+    public static final Level OFF = (Level)(Object) new TLevel("OFF",Integer.MAX_VALUE, null);
+
+    public static final Level SEVERE = (Level)(Object) new TLevel("SEVERE",1000, null);
+
+    public static final Level WARNING = (Level)(Object) new TLevel("WARNING", 900, null);
+
+    public static final Level INFO = (Level)(Object) new TLevel("INFO", 800, null);
+
+    public static final Level CONFIG = (Level)(Object) new TLevel("CONFIG", 700, null);
+
+    public static final Level FINE = (Level)(Object) new TLevel("FINE", 500, null);
+
+    public static final Level FINER = (Level)(Object) new TLevel("FINER", 400, null);
+
+    public static final Level FINEST = (Level)(Object) new TLevel("FINEST", 300, null);
+
+    public static final Level ALL = (Level)(Object) new TLevel("ALL", Integer.MIN_VALUE, null);
+
+    private final String name;
+    private final int value;
+    private final String resourceBundleName;
+
+    private TLevel(final String name, final int value, final String resourceBundleName) {
+        if (name == null) {
+            throw new NullPointerException();
+        }
+        this.name = name;
+        this.value = value;
+        this.resourceBundleName = resourceBundleName;
+    }
+
+    public int intValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ core/src/test/java/de/mirkosertic/bytecoder/complex/JMonkeyEngineTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Mirko Sertic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.mirkosertic.bytecoder.complex;
+
+import com.jme3.math.Vector3f;
+import de.mirkosertic.bytecoder.unittest.BytecoderUnitTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RunWith(BytecoderUnitTestRunner.class)
+public class JMonkeyEngineTest {
+
+    @Test
+    public void testVector() {
+        Vector3f v = Vector3f.UNIT_X;
+        v = v.add(0f, 2f, 0f);
+        v.normalizeLocal();
+    }
+
+    @Test
+    public void testLogger() {
+        final Logger logger = Logger.getLogger("Testlogger");
+        logger.setLevel(Level.OFF);
+        logger.config("config");
+        logger.fine("fine");
+        logger.finer("finer");
+        logger.finest("finest");
+        logger.info("info");
+        logger.severe("severe");
+        logger.warning("warning");
+    }
+}
