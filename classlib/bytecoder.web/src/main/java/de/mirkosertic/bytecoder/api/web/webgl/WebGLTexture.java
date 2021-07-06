a56b9666505ad0bb91363a10b08de1d0c77eb090
Yuri Pourre <yuripourre@users.noreply.github.com>
2021-07-06 09:16:30 -0300

    Add WebGL classes (#592)
    * Add webgl classes
    * Add HTMLWebGLCanvasElement

---
+++ classlib/bytecoder.web/src/main/java/de/mirkosertic/bytecoder/api/web/webgl/WebGLTexture.java
@@ -0,0 +1,7 @@
+package de.mirkosertic.bytecoder.api.web.webgl;
+
+import de.mirkosertic.bytecoder.api.OpaqueReferenceType;
+
+public class WebGLTexture implements OpaqueReferenceType {
+
+}
---
+++ classlib/bytecoder.web/src/main/java/de/mirkosertic/bytecoder/api/web/webgl/WebGLUniformLocation.java
@@ -0,0 +1,7 @@
+package de.mirkosertic.bytecoder.api.web.webgl;
+
+import de.mirkosertic.bytecoder.api.OpaqueReferenceType;
+
+public class WebGLUniformLocation implements OpaqueReferenceType {
+
+}
