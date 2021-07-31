c4b4ce1802cd71137c3ed1a2fb19f174f8da8730
Yuri Pourre <yuripourre@users.noreply.github.com>
2021-07-31 05:55:57 -0300

    Add methods to get width/height of Elements (#607)

--- classlib/bytecoder.web/src/main/java/de/mirkosertic/bytecoder/api/web/HTMLWebGLCanvasElement.java
+++ classlib/bytecoder.web/src/main/java/de/mirkosertic/bytecoder/api/web/HTMLWebGLCanvasElement.java
@@ -1,8 +1,15 @@
 package de.mirkosertic.bytecoder.api.web;
 
+import de.mirkosertic.bytecoder.api.OpaqueProperty;
 import de.mirkosertic.bytecoder.api.web.webgl.WebGLRenderingContext;
 
 public interface HTMLWebGLCanvasElement extends HTMLElement {
+
+    @OpaqueProperty
+    int width();
+
+    @OpaqueProperty
+    int height();
 
     WebGLRenderingContext getContext(String context);
 
