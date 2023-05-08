4bbede2b3e09082f0d8db2480b8043f93ad15015
mirkosertic <mirko.sertic@web.de>
2023-05-08 19:54:09 +0200

    Restructured generated JS code as a foundation for further optimizations.

--- core/src/main/java/de/mirkosertic/bytecoder/core/backend/VTable.java
+++ core/src/main/java/de/mirkosertic/bytecoder/core/backend/VTable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.mirkosertic.bytecoder.core.backend.wasm;
+package de.mirkosertic.bytecoder.core.backend;
 
 import de.mirkosertic.bytecoder.core.ir.ResolvedMethod;
 
