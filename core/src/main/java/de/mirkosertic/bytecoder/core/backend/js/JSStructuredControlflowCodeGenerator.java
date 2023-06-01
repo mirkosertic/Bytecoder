b3f96db26256ffbd3fd16f0ce01d46fd80bda2ce
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2023-06-01 21:47:35 +0200

    fix(core): Jump labels contain invalid -
    ref: #927

--- core/src/main/java/de/mirkosertic/bytecoder/core/backend/js/JSStructuredControlflowCodeGenerator.java
+++ core/src/main/java/de/mirkosertic/bytecoder/core/backend/js/JSStructuredControlflowCodeGenerator.java
@@ -1988,7 +1988,7 @@ public class JSStructuredControlflowCodeGenerator implements StructuredControlfl
     @Override
     public void startBlock(final Sequencer.Block block) {
         writeIndent();
-        pw.print(block.label);
+        pw.print(block.label.replace("-",""));
         pw.print(": ");
         if (block.type == Sequencer.Block.Type.LOOP) {
             pw.print("while(true) ");
@@ -2066,7 +2066,7 @@ public class JSStructuredControlflowCodeGenerator implements StructuredControlfl
     public void writeBreakTo(final String label) {
         writeIndent();
         pw.print("break ");
-        pw.print(label);
+        pw.print(label.replace("-",""));
         pw.println(";");
     }
 
@@ -2074,7 +2074,7 @@ public class JSStructuredControlflowCodeGenerator implements StructuredControlfl
     public void writeContinueTo(final String label) {
         writeIndent();
         pw.print("continue ");
-        pw.print(label);
+        pw.print(label.replace("-",""));
         pw.println(";");
     }
 
